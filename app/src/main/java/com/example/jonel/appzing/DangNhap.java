6be7b308b5ac613c62d3fb21d4e704af3aacc83b
tranngocthang151250533242 <joneltran@gmail.com>
2017-10-10 17:18:19 +0700

    Update code Ngoc Thang

---
+++ app/src/main/java/com/example/jonel/appzing/DangNhap.java
@@ -0,0 +1,72 @@
+package com.example.jonel.appzing;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.jonel.appzing.main.MainActivity;
+import com.example.jonel.appzing.main.register;
+import com.example.jonel.appzing.model.SQLite;
+
+public class DangNhap extends AppCompatActivity {
+    private Button btnDN, regiter, qmk;
+    private EditText user,pass;
+    private SQLite db = new SQLite(this);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dang_nhap);
+        _anhxa();
+        _Click();
+        Toast.makeText(this, "Chào bạn đến với ứng dụng!!!", Toast.LENGTH_SHORT).show();
+    }
+    public void _anhxa(){
+        user = (EditText) findViewById(R.id.user);
+        pass = (EditText) findViewById(R.id.password);
+        btnDN = (Button) findViewById(R.id.btnDN);
+        regiter = (Button) findViewById(R.id.regiter);
+    }
+//    public boolean Login(String username, String password) {
+//        Cursor mCursor = db.GetData("SELECT * FROM ThanhVien WHERE MaTV like "+username+" AND password="+password);
+//        if (mCursor != null) {
+//            if(mCursor.getCount() > 0)
+//            {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+    public boolean Login1(String username, String password){
+        String a="";
+        String b="";
+        if (username.equals(a) && password.equals(b))
+            return true;
+        else return false;
+    }
+    public void _Click(){
+        btnDN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tk = user.getText().toString();
+                String pw = pass.getText().toString();
+                if (Login1(tk,pw) == true){
+                    Intent trangChu = new Intent(DangNhap.this, MainActivity.class);
+                    startActivity(trangChu);
+                }else Toast.makeText(DangNhap.this, "Lỗi đăng nhập", Toast.LENGTH_SHORT).show();
+            }
+        });
+        // sự kiện register
+        regiter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent DangKy = new Intent(DangNhap.this, register.class);
+                startActivity(DangKy);
+            }
+        });
+        // sự kiện quên mật khẩu
+    }
+}
--- app/src/main/java/com/example/jonel/appzing/main/MainActivity.java
+++ app/src/main/java/com/example/jonel/appzing/main/MainActivity.java
@@ -1,27 +1,42 @@
 package com.example.jonel.appzing.main;
 
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.jonel.appzing.DangNhap;
 import com.example.jonel.appzing.R;
-import com.example.jonel.appzing.model.SQLite;
+
+import static com.example.jonel.appzing.R.id.btnChuyen;
 
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnDN, regiter, qmk;
-    private EditText user,pass;
-    private SQLite db = new SQLite(this);
-
+    private Button next;
+    private TextView imgBaiHat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
         _anhxa();
         _Click();
 //        _run();
@@ -35,50 +50,27 @@ public class MainActivity extends AppCompatActivity {
         // sự kiện đăng nhập
 
     }
-    public boolean Login(String username, String password) {
-        Cursor mCursor = db.GetData("SELECT * FROM ThanhVien WHERE MaTV like "+username+" AND password="+password);
-        if (mCursor != null) {
-            if(mCursor.getCount() > 0)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-    public boolean Login1(String username, String password){
-        String a="";
-        String b="";
-        if (username.equals(a) && password.equals(b))
-            return true;
-        else return false;
-    }
     public void _Click(){
-        btnDN.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String tk = user.getText().toString();
-                String pw = pass.getText().toString();
-                if (Login1(tk,pw) == true){
-                    Intent trangChu = new Intent(MainActivity.this, trangChu.class);
-                    startActivity(trangChu);
-                }else Toast.makeText(MainActivity.this, "Lỗi đăng nhập", Toast.LENGTH_SHORT).show();
-            }
-        });
         // sự kiện register
-        regiter.setOnClickListener(new View.OnClickListener() {
+        next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent DangKy = new Intent(MainActivity.this, register.class);
+                Intent DangKy = new Intent(MainActivity.this, DangNhap.class);
                 startActivity(DangKy);
             }
         });
+        imgBaiHat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent DangKy = new Intent(MainActivity.this, DangNhap.class);
+                startActivity(DangKy);
+            }
         // sự kiện quên mật khẩu
+    });
     }
     public void _anhxa(){
-        user = (EditText) findViewById(R.id.user);
-        pass = (EditText) findViewById(R.id.password);
-        btnDN = (Button) findViewById(R.id.btnDN);
-        regiter = (Button) findViewById(R.id.regiter);
+        next = (Button) findViewById(btnChuyen);
+        imgBaiHat = (TextView) findViewById(R.id.tvBaiHat);
     }
     // Đọc file từ internet
 //    private String docNoiDung_Tu_URL(String theUrl){
@@ -125,4 +117,24 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
 //    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
